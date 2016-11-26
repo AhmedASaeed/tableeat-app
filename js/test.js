9693c0b964f7aa51ba0ada581c1eac8f8ed8d5f0
Ahmed Abdo <hmdabdo3@gmail.com>
2016-11-26 21:54:25 +0900

    first commit

---
+++ js/test.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$('#login').on('submit',function(e){
+		var email =$('#email').val();
+		var data;
+		alert(email);
+		var posts =$.getJSON("http://52.79.42.151:3001/api/users", function(result){
+	        $.each(result, function(i, field){
+	            data.append(i.name + " ");
+	        });alret(data)
+	    });
+		
+	});
+});
+
+
+
+
+
+
+

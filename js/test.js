e6352704344c7f9d236b74fabf4ee574e9a48f92
atassiam <ammar.atassi1@gmail.com>
2016-11-28 01:38:52 +0900

    menu + search page

--- js/test.js
+++ js/test.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('#login').on('submit',function(e){
 		var email =$('#email').val();
 		var data;
 		alert(email);
-		var posts =$.getJSON("http://52.79.42.151:3001/api/users", function(result){
+		var posts =$.getJSON("http://52.79.42.151:3000/api/login", function(result){
 	        $.each(result, function(i, field){
 	            data.append(i._id + " ");
 	        });alret(data)
@@ -11,10 +11,20 @@ $(document).ready(function(){
 		
 	});
 });
+*/
+var email = $('#email').val();
+var password = $('#pass').val();
+var params = { "username": email, "password": password };
 
-
-
-
-
-
-
+$.ajax({
+    type: "POST",
+    url: "http://52.79.42.151:3000/api/login",
+    // The key needs to match your method's input parameter (case-sensitive).
+    data: JSON.stringify({ Markers: markers }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){alert(data);},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});

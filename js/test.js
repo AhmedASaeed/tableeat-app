098b45192bfa1dd6414cf8361a9458529e7c562d
Ahmed Abdo <hmdabdo3@gmail.com>
2016-11-30 16:50:19 +0900

    new login

--- js/test.js
+++ js/test.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
 	$('#login').on('submit',function(e){
 		var email =$('#email').val();
+		var password =$('#pass').val();
 		var data;
 		alert(email);
-		var posts =$.getJSON("http://52.79.42.151:3001/api/users", function(result){
-	        $.each(result, function(i, field){
-	            data.append(i._id + " ");
-	        });alret(data)
-	    });
+		$.ajax({
+		    type: "POST",
+		    url: "http://52.79.42.151:3000/api/login",
+		    // The key needs to match your method's input parameter (case-sensitive).
+		    data: JSON.stringify({ "username": email,"password":password}),
+		    contentType: "application/json; charset=utf-8",
+		    dataType: "text",
+		    success: function(data){alert(data);},
+		    failure: function(errMsg) {
+		        alert(errMsg);
+		    }
+		});
 		
 	});
 });

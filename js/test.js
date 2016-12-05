aa28286e28666776f2bec84249ee2076f5ba84d9
atassiam <ammar.atassi1@gmail.com>
2016-12-05 19:05:27 +0900

    login and resister (redirect doesn't work

--- js/test.js
+++ js/test.js
@@ -1,82 +1,62 @@
-/*$(document).ready(function(){
-	$('#login').on('submit',function(e){
-		var email =$('#email').val();
-		var password =$('#pass').val();
-		var data;
-		alert(email);
-<<<<<<< HEAD
-		$.ajax({
-		    type: "POST",
-		    url: "http://52.79.42.151:3000/api/login",
-		    // The key needs to match your method's input parameter (case-sensitive).
-		    data: JSON.stringify({ "username": email,"password":password}),
-		    contentType: "application/json; charset=utf-8",
-		    dataType: "text",
-		    success: function(data){alert(data);},
-		    failure: function(errMsg) {
-		        alert(errMsg);
-		    }
-		});
-=======
-		var posts =$.getJSON("http://52.79.42.151:3000/api/login", function(result){
-	        $.each(result, function(i, field){
-	            data.append(i._id + " ");
-	        });alret(data)
-	    });
->>>>>>> e6352704344c7f9d236b74fabf4ee574e9a48f92
-		
-	});
-});
-*/
-/*
-var email = $('#email').val();
-var password = $('#pass').val();
-var params = { "username": email, "password": password };
-
-$.ajax({
-    type: "POST",
-    url: "http://52.79.42.151:3000/api/login",
-    // The key needs to match your method's input parameter (case-sensitive).
-    data: JSON.stringify(params),
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    success: function(data){alert(data);},
-    failure: function(errMsg) {
-        alert(errMsg);
-    }
-});
-*/
 $(document).ready(function() {
-    // Lorsque je soumets le formulaire
-    $('#signup-form').on('submit', function(e) {
-        e.preventDefault(); // J'empêche le comportement par défaut du navigateur, c-à-d de soumettre le formulaire
- 
-        var $this = $(this); // L'objet jQuery du formulaire
- 
-        // Je récupère les valeurs
-        var firstname = $('#firstname').val();
-        var lastname = $('#lastname').val();
-        var username = $('#username').val();
-        var password = $('#password').val();
- 
-        // Je vérifie une première fois pour ne pas lancer la requête HTTP
-        // si je sais que mon PHP renverra une erreur
-        if(username === '' || password === '') {
-            alert('Fields must be completed');
-        } else {
-            // Envoi de la requête HTTP en mode asynchrone
-            $.ajax({
-                url: $this.attr('action'), // Le nom du fichier indiqué dans le formulaire
-                type: $this.attr('method'), // La méthode indiquée dans le formulaire (get ou post)
-                data: $this.serialize(),
-                dataType: 'json', // JSON// Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
-                success: function(json) {
-                    if(json.reponse === 'ok') {
-                        alert('Tout est bon');
-                    } else {
-                        alert('Erreur : '+ json.reponse);
-                    }
-            });
-        }
-    });
+	// When I submit the form
+	$('#signup-form').on('submit', function(e) {
+		e.preventDefault(); // I prevent the default behavior of the browser, ie
+		// submit the form
+
+		var $this = $(this); // jQuery object of the form
+
+		// Get the values
+		var firstname = $('#firstname').val();
+		var lastname = $('#lastname').val();
+		var username = $('#username').val();
+		var password = $('#password').val();
+
+		// I check the first time to not run the HTTP request
+		// if I know my PHP will return an error
+
+		if (username === '' || password === '' || firstname === '' || lastname === '') {
+			alert('Fields must be completed');
+		} else {
+			// Sending the HTTP request in asynchronous mode
+			$.ajax({
+				url : $this.attr('action'), // The name of the file indicated in the form
+				type : $this.attr('method'), // The method specified in the form (get or post)
+				data : $this.serialize(),
+				dataType : 'json', // JSON// I serialize the data (I send all the values ​​present in the form)
+				success : function() {
+					window.location.href = "http://google.com";
+				}
+			});
+		}
+	});
+
+	$('#login-form').on('submit', function(e) {
+		e.preventDefault(); // I prevent the default behavior of the browser, ie submit the form
+
+		var $this = $(this); // jQuery object of the form
+
+		// Get the values
+		var username = $('#usernamel').val();
+		var password = $('#passwordl').val();
+
+		// I check the first time to not run the HTTP request
+		// if I know my PHP will return an error
+		
+		if (username === '' || password === '') {
+			alert('Fields must be completed');
+		} else {
+			// Sending the HTTP request in asynchronous mode
+			$.ajax({
+				url : $this.attr('action'), // The name of the file indicated in the form
+				type : $this.attr('method'), // The method specified in the // form (get or post)
+				data : $this.serialize(),
+				dataType : 'json', // JSON// I serialize the data (I send all
+				// the values ​​present in the form)
+				success : function() {
+					window.location.href = "http://google.com";
+				}
+			});
+		}
+	});
 });
